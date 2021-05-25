8529be3f26edb2eb123cbad9920dcf443cf880dc
Jan Schutte <jan.o.schutte@gmail.com>
2021-05-25 21:50:44 +0200

    Add plot and benchmark output

--- plot.py
+++ plot.py
@@ -36,13 +36,29 @@ def parse_output(file: Path, x_key='N', y_key='time per step'):
 
 
 def plot():
-    x, y = parse_output(Path('./output'), 'N', 'frames per second')
+    x, y_total = parse_output(Path('./output'), 'N', 'total step')
+    _, y_advect = parse_output(Path('./output'), 'N', 'advect')
+    _, y_linsolve = parse_output(Path('./output'), 'N', 'lin_solve')
+    _, y_project = parse_output(Path('./output'), 'N', 'project')
+    _, y_source = parse_output(Path('./output'), 'N', 'add_source')
     
     plt.figure()
-    plt.plot(x, y) 
-    plt.ylabel('Frames per second')
-    plt.xlabel('Grid size')
+    # plt.plot(x, y_total, label='total')
+    plt.plot(x, y_advect, label='advect')
+    # plt.plot(x, y_linsolve, label='lin_solve')
+    # plt.plot(x, y_project, label='project')
+    plt.plot(x, y_source, label='add_source')
+    
+    plt.plot(x, [a - b for a, b in zip (y_project,y_linsolve)], label='project overhead')
+
+    # Plot what we want
+    plt.plot(x, [16.7] * len(x), label='60 FPS', linestyle='--')
+    plt.plot(x, [6.9] * len(x), label='144 FPS', linestyle='--')
+
+    plt.ylabel('Step time (ms)')
+    plt.xlabel('Grid size (N)')
     plt.xticks(x, x)
+    plt.legend()
     plt.savefig('plot.png')
 
 
